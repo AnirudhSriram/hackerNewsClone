f73c2e3754b2596cf077fba41eca08cbcde48e5f
Anirudh Sriram <anirudh.sriram@siemensgamesa.com>
2019-10-23 15:31:20 +0530

    Implementation with context api

---
+++ src/search.js
@@ -0,0 +1,32 @@
+import React ,{useState,useEffect} from 'react';
+import MyContext from './context';
+// const Search = () => {
+//     const [query,search] = useState('');
+//     const filter = (event)=> {
+//         search(event.target.value)
+//     }
+//     useEffect(()=> {
+//         console.log(query);
+//     })
+    
+//     return   <input className="searchBar" value={query} placeholder="search" onChange={(event)=>filter(event)}/>
+// }
+
+class Search extends React.Component{
+    render(){
+        
+        return (
+            <div>
+                <MyContext.Consumer>
+                    {(context)=> (
+                        <React.Fragment>
+                            <input className="searchBar" value={context.state.searchQuery} placeholder="search" onChange={(event) => context.setSearchQuery(event.target.value)}/>
+                        </React.Fragment>
+                    )}
+                </MyContext.Consumer>
+            </div>
+        )
+    }
+}
+
+export default Search;
--- src/topics.js
+++ src/topics.js
@@ -1,61 +1,81 @@
-import React  from 'react';
+import React from 'react';
 import axios from 'axios';
 import './topics.css';
 import Topic from './topic';
+import MyContext from './context';
 class Topics extends React.Component {
-    constructor(props){
-            super(props);
-            this.state = {
-                topics : []
-            }
+    constructor(props) {
+        super(props);
+        this.state = {
+            topics: [],
+            random: true
+        }
     }
+    componentDidUpdate(prevProps, prevState) {
+        if (this.context.state.topics != prevState.topics) {
+            this.setState({
+                topics: this.context.state.topics
+            })
+        }
 
-    componentDidMount(){
+    }
+    componentWillMount() {
+
+        Topics.contextType = MyContext;
         let ids = []
+
         axios
             .get("https://hacker-news.firebaseio.com/v0/topstories.json")
             .then(result => {
                 ids = result.data;
-                ids.splice(10,ids.length-1);
+                ids.splice(10, ids.length - 1);
             })
-            .then(()=>{
-                ids.forEach((item)=> {
+            .then(() => {
+                ids.forEach((item) => {
                     axios.get(`https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`)
-                    .then(result => {
-                        const temp = this.state.topics;
-                        temp.push(result.data);
-                        this.setState({
-                            topics:temp
+                        .then(result => {
+                            let temp = this.context.state.topics;
+                            temp.push(result.data)
+                            this.setState({
+                                topics: temp
+                            })
+
+                            this.context.setTopics(temp)
+
+                            //  this.context.setTopics(temp)
+                            // this.context.setTopics(temp)
                         })
-                    })
                 })
             })
             .catch(err => {
                 this.setState({
-                    err : err})
-            }).finally(()=>{
-                
+                    err: err
+                })
+            }).finally(() => {
+                this.setState({
+                    random: !this.state.random
+                })
             });
     }
-    
-    // useEffect(()=> {
-    //     
-            
-    // },[])
 
-    render(){
+
+
+
+
+    render() {
+
         var render = []
         this.state.topics.forEach(item => {
-            render.push(<li key={item.id}><Topic  details={item} /></li>)
+            render.push(<li key={item.id}><Topic details={item} /></li>)
         });
         return (<div>
             <h1 className="title">Topics</h1>
             <ul className="ul">
-               {render}
+                {render}
             </ul>
         </div>)
     }
-    
+
 }
 
 export default Topics;
